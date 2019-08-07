4e2d4b19baff8d44b2bd239f14e5fa6a070f7d31
libo8621696 <b_li08@163.com>
2019-08-08 05:41:25 +0800

    third commit with gc files modified in accorance with the starter code

--- gc_details.h
+++ gc_details.h
@@ -5,46 +5,42 @@ template <class T>
 class PtrDetails
 {
   public:
-    // current reference count
-    unsigned refcount;
-
-    // pointer to allocated memory
-    T *memPtr;
-
+    unsigned refcount; // current reference count
+    T *memPtr;         // pointer to allocated memory
     /* isArray is true if memPtr points
-       to an allocated array. It is false
-       otherwise. */
+to an allocated array. It is false
+otherwise. */
     bool isArray; // true if pointing to array
-
     /* If memPtr is pointing to an allocated
-       array, then arraySize contains its size */
+array, then arraySize contains its size */
     unsigned arraySize; // size of array
-
-    // Constructor.  Here mPtr points to the allocated
-    // memory. If this is an array, then size specifies
+    // Here, mPtr points to the allocated memory.
+    // If this is an array, then size specifies
     // the size of the array.
+
     PtrDetails(T* m_ptr, unsigned size=0)
     {
+        // TODO: Implement PtrDetails
         // Set reference count to newly allocated memory 1.
         refcount = 1;
 
         // Assign memPtr to address of newly allocated memory.
         memPtr = m_ptr;
 
-        // If size > 0 then set member variable indicating
-        // newly allocated memory is associated with an array.
-        if (size != 0) isArray = true;
+        // If size > 0 then set member variable indicating newly allocated memory is associated with an array.
+        if(size != 0) isArray = true;
         else isArray = false;
 
         arraySize = size;
+
     }
 };
-
 // Overloading operator== allows two class objects to be compared.
 // This is needed by the STL list class.
 template <class T>
 bool operator==(const PtrDetails<T> &ob1,
                 const PtrDetails<T> &ob2)
 {
+    // TODO: Implement operator==
     return (ob1.memPtr == ob2.memPtr);
 }
